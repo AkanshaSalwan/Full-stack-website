8cb060a3ca8305d5464d99efe068dbef95d8cf51
Akansha Salwan <akankshasalwan@gmail.com>
2024-10-13 15:48:21 +0530

    frontend updates

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYJ3LuMjo-9bwmZthvV4TKbwiySUdX1Lo",
+  authDomain: "full-stact-website.firebaseapp.com",
+  projectId: "full-stact-website",
+  storageBucket: "full-stact-website.appspot.com",
+  messagingSenderId: "526390710820",
+  appId: "1:526390710820:web:729634afb8aa3da19afeb1"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
